e2faba24f482380c2f33709db8115621f025f3c8
nastya3781 <116462454+nastya3781@users.noreply.github.com>
2023-03-11 15:45:08 +0700

    Функция по созданию таблицы в MS SQL

---
+++ TG_bot_IS/create_table.py
@@ -0,0 +1,15 @@
+import pyodbc
+
+def create_table(server_name):
+    connection_db = pyodbc.connect(r'Driver={SQL Server};Server='+server_name+';Database=TGbot;Trusted_Connection=yes;')
+    curs = connection_db.cursor()
+    curs.execute('''IF NOT EXISTS 
+                        (
+                        SELECT *
+                        FROM sysobjects
+                        WHERE name='Users')
+                        CREATE TABLE Users (
+                        UserID bigint NOT NULL, TelegramID bigint NOT NULL, ChatID bigint NOT NULL, corp_email text, created_date datetime
+                        )
+                        ''')
+    connection_db.commit()
