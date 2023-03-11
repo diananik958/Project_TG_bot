5eed9cebabbf6ff936f8227f3e54f824e60d42a1
diananik958 <diananikkel958@gmail.com>
2023-03-11 13:17:27 +0700

    создала файл main.py и добавила класс app

---
+++ TG_bot_IS/main.py
@@ -0,0 +1,8 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import BotCommand
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+
+class App:
+    def __int__(self):
+        pass
