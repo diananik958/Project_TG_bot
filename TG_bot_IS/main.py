699265b5e0bd2fe93f039a44df3a69dac0a4dc9c
nastya3781 <matyunckina.an@yandex.ru>
2023-03-15 00:32:22 +0700

    Немного подкорректировала текст, требуются еще доработки

--- TG_bot_IS/main.py
+++ TG_bot_IS/main.py
@@ -1,5 +1,5 @@
 import logging
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher, executor
 from aiogram import types
 from datetime import datetime
 from dotenv import load_dotenv
@@ -33,12 +33,13 @@ async def send_welcome(message: types.Message):
     if user_exists == 'None':
         try:
             add_user(message.from_user.id, message.from_user.id, message.chat.id)
-            await message.answer("Добрый день\nЯ бот, который должен помочь тебе повысить знания в фишинге."
-                                 "\nНо для начала тебе нужно зарегестрироваться")
+            await message.answer(f"Добрый день, {message.from_user.first_name}!\nЯ бот, который должен помочь тебе повысить знания в фишинге."
+                                 "\nНо для начала тебе нужно зарегистрироваться.\nДля этого введите команду /reg"
+                                 " в формате /reg ваш_адрес@nstu.ru")
         except AppExceptions.UserAlreadyExist as error:
             print(error)
             await message.answer(
-                "Добрый день\nЯ бот, который должен помочь тебе повысить знания в фишинге."
+                f"Добрый день, {message.from_user.first_name}!Я бот, который должен помочь тебе повысить знания в фишинге."
                                  "\nЧто-то пошло не так.")
         except Exception as e:
             print(e)
@@ -47,7 +48,7 @@ async def send_welcome(message: types.Message):
                 "Добрый день\nЯ бот, который должен помочь тебе повысить знания в фишинге."
                 "\nЧто-то пошло не так...")
     else:
-        await message.answer("Добрый день\nЯ бот, который должен помочь тебе повысить знания в фишинге."
+        await message.answer(f"Добрый день, {message.from_user.first_name}!\nЯ бот, который должен помочь тебе повысить знания в фишинге."
                              "\nЕсли нужна помощь нажмите /help")
 
 # HELP---------------------------------------------------------------------------------------------
@@ -55,7 +56,7 @@ async def send_welcome(message: types.Message):
 @dp.message_handler(commands=['help'])
 async def helpMSG(message: types.Message):
     text = (
-        "/reg - команда для регистрации в боте. Для начала обучения необходимо зарегестрироваться. Введите команду "
+        "/reg - команда для регистрации в боте. Для начала обучения необходимо зарегистрироваться. Введите команду "
         "в формате /reg ваш_адрес@nstu.ru\n"
         "/theory - команда для изучения теории по теме\n"
         "/test - команда для изучения прохождения теста по теме\n"
